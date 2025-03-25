413979dd2ce4ca460f2e5e2fa2dbc6038174428b
saeed <git@saeedc.com>
2025-03-24 20:35:02 -0400

    hyprland specific packages

--- modules/packages.nix
+++ modules/packages.nix
@@ -5,21 +5,27 @@
   ...
 }:
 {
-  environment.systemPackages = with pkgs; [
-    clang
-    curl
-    discord
-    drawio
-    fastfetch
-    fd
-    fzf
-    keepassxc
-    mold
-    mpv
-    obsidian
-    pinentry-curses
-    ripgrep
-    rsync
-    tree
-  ];
+  environment.systemPackages =
+    with pkgs;
+    [
+      clang
+      curl
+      discord
+      drawio
+      fastfetch
+      fd
+      fzf
+      kdePackages.okular
+      keepassxc
+      mold
+      mpv
+      obsidian
+      pinentry-curses
+      ripgrep
+      rsync
+      tree
+    ]
+    ++ lib.optionals config.isHyprland [
+      xfce.ristretto
+    ];
 }
